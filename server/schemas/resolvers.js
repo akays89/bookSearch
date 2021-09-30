6631aff4755b5e3faaa8c8cae21697c18eed5909
Amanda Kays <amandakays89@gmail.com>
2021-09-30 14:23:17 -0500

    Added a mutation to remove a book

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -53,8 +53,20 @@ const resolvers = {
             throw new AuthenticationError('You must be logged in!')
         },
 
+        removeBook: async (parent, args, context) => {
+            if (context.user) {
+                const updatedUser = await User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    { $pull: { savedBooks: { bookId: args.bookId } } },
+                    { new: true }
+                );
+                return updatedUser;
+            }
+            throw new AuthenticationError('You must be logged in!')
+        }
+
     }
-}
+};
 
 
   
