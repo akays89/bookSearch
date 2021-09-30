e4ebf208d9651b105ad641e7363202d48a329006
Amanda Kays <amandakays89@gmail.com>
2021-09-29 22:28:51 -0500

    a query for user to log in

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -4,8 +4,15 @@ const { signToken } = require("../utils/auth");
 
 const resolvers = {
     Query: {
-      helloWorld: () => {
-        return "Hello World!";
+        me: async (parent, args, context) => {
+            if (context.user) {
+              const userData = await User.findOne({ _id: context.user._id }).select(
+                "-__v -password"
+              );
+      
+              return userData;
+            }
+            throw new AuthenticationError("Not logged in!");
       },
     },
   };
