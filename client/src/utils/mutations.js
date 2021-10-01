5d9927fbe63a9a8158538922554f57fa7a586648
Amanda Kays <amandakays89@gmail.com>
2021-10-01 11:18:46 -0500

    sane book mutation

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -44,3 +44,21 @@ export const LOGIN_USER = gql`
     }
 }
 `;
+
+export const SAVE_BOOK = gql`
+    mutation saveBook($input: bookInput!) {
+        saveBook(input: $input) {
+            _id
+            username
+            email
+            savedBooks {
+                bookId
+                authors
+                image
+                description
+                title
+                link
+            }
+        }
+    }
+`;
